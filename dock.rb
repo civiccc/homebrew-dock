8a3396473546667faf0c87b222887055c49dbdda
Shane da Silva <shane@dasilva.io>
2016-11-23 23:40:12 -0800

    Update formula for Dock 1.3.0

--- dock.rb
+++ dock.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Dock < Formula
   homepage 'https://github.com/brigade/dock'
-  url 'https://github.com/brigade/dock/archive/v1.2.0.tar.gz'
-  sha256 '4005bc2577d926515705908a77d6721e30212c2e7d81838ad7f44efc204aef8f'
+  url 'https://github.com/brigade/dock/archive/v1.3.0.tar.gz'
+  sha256 'c0347d915843e0c4fdba03c4934d0177f12fd1c2a5d4abee9572919b7b022a38'
 
   def install
     bin.install File.join('bin', 'dock')
