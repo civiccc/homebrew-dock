49ea2bfc7c18cbbb20815d6fddb2457004004a5b
Anuj Das <anujdas@gmail.com>
2017-03-22 13:39:15 -0700

    Fix SHA256 for dock 1.4.1 formula

--- dock.rb
+++ dock.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Dock < Formula
   homepage 'https://github.com/brigade/dock'
   url 'https://github.com/brigade/dock/archive/v1.4.1.tar.gz'
-  sha256 '4b4cf2c3b6d41e81be944a6f480846ed87403aa5b55e13dafb86624b452f9ed3'
+  sha256 '89a384fe052254383c4e29a158d0a18e85d1fff4e8fff1d9db12ceb57f94733c'
 
   def install
     bin.install File.join('bin', 'dock')
